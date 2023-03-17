a94564afc34c83a023cbefcf5489a9a1dea377e6
JessicaJulieth <3497@holbertonschool.com>
2023-03-16 21:25:14 -0500

    Add

--- 0x01-dart-function/10-longest-palindrome.dart
+++ 0x01-dart-function/10-longest-palindrome.dart
@@ -1,13 +1,30 @@
-String longestPalindrome(String s) {
-  if (s.length < 3) return 'none';
+String getSubStr(String str, int low, int high) {
+  String d = "";
+  for (int i = low; i <= high; ++i) d += str[i];
+  if (d.length < 3) {
+    return "none";
+  }
+  return d;
+}
 
-  String long = '';
-  String current = '';
+String longestPalindrome(String str) {
+  int len = str.length;
+  int maxLength = 1, start = 0;
+  if (len < 3) {
+    return "none";
+  }
+  int flag = 1;
+  for (int i = 0; i < len; i++) {
+    for (int j = i; j < len; j++) {
+      flag = 1;
+      for (int k = 0; k < (j - i) / 2; k++)
+        if (str[i + k] != str[j - k]) flag = 0;
 
-  for (int i = 0; i < s.length; i++) {
-    for (int j = i; j < s.length; j++) {
-      current = s.substring(i, j + 1);
+      if (flag != 0 && (j - i) > maxLength) {
+        start = i;
+        maxLength = j - i + 1;
+      }
     }
   }
-  return long;
+  return (getSubStr(str, start, start + maxLength - 1));
 }
